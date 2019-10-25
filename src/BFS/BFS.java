5c5fa2b1c757c5ddd990d080e104fea858d9ba24
Matúš Chládek <matus.chladek@exponea.com>
2019-10-25 11:35:19 +0200

    BFS init

--- src/BFS/BFS.java
+++ src/BFS/BFS.java
@@ -1,4 +1,32 @@
 package BFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS {
+    public void bfs(Vertex root) {
+
+        // we need FIFO structure
+        Queue<Vertex> queue = new LinkedList<>();
+
+        root.setVisited(true);
+        queue.add(root);
+
+
+        while (!queue.isEmpty()) {
+            /*
+             * Retrieves and removes head of the queue
+             */
+            Vertex actualVertex = queue.remove();
+            System.out.println(actualVertex + "");
+
+            for (Vertex v : actualVertex.getNeighbourList()) {
+                if (!v.isVisited()) {
+                    v.setVisited(true);
+                    queue.add(v);
+                }
+            }
+        }
+
+    }
 }
